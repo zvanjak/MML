0b01e4776cc075ba419d3e2a9ab06b5a9e046d28
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2024-02-25 15:59:59 +0100

    RELEASE 0.9

--- include/MML.h
+++ include/MML.h
@@ -51,9 +51,21 @@ static const std::string GLOB_PATH_VectorFieldViz     = "E:\\Projects\\MinimalMa
 
 
 // https://opensource.apple.com/source/CarbonHeaders/CarbonHeaders-18.1/TargetConditionals.h.auto.html
+#ifdef __APPLE__
+#  include <TargetConditionals.h>
+#  if (defined(TARGET_OS_OSX) && TARGET_OS_OSX == 1) || \
       (defined(TARGET_OS_MAC) && TARGET_OS_MAC == 1)
+#    define MML_PLATFORM_MAC
+#  elif (defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE == 1)
+#    define MML_PLATFORM_IPHONE
+#  endif
 
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+#  define MML_PLATFORM_LINUX
 
+#elif defined(WIN32) || defined(__WIN32__) || defined(_WIN32) || defined(_MSC_VER) || defined(__MINGW32__)
+#  define MML_PLATFORM_WINDOWS
+#endif
 
 
 namespace MML
