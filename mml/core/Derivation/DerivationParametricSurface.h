454c60ec74529e6f98aa70aee16fd364bb1045e7
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2026-03-22 10:13:15 +0100

    RELEASE 1.2.1

--- mml/core/Derivation/DerivationParametricSurface.h
+++ mml/core/Derivation/DerivationParametricSurface.h
@@ -143,11 +143,6 @@ namespace MML
 		}
 
 		template <int N>
-		static VectorN<Real, N> NDer2_uw(const IParametricSurfaceRect<N>& f, Real u, Real w, Real h, Real* error = nullptr)
-		{
-			return NDer2_uw<N>(f, u, w, h, h, error);
-		}
-		template <int N>
 		static VectorN<Real, N> NDer2_uw(const IParametricSurfaceRect<N>& f, Real u, Real w, Real h_u, Real h_w, Real* error = nullptr)
 		{
 			// Mixed partial derivative: ∂²f/∂u∂w
@@ -165,6 +160,11 @@ namespace MML
 				*error = norm_sum * Constants::Eps / (4 * h_u * h_w);
 			}
 			return diff / (4 * h_u * h_w);
+		}
+		template <int N>
+		static VectorN<Real, N> NDer2_uw(const IParametricSurfaceRect<N>& f, Real u, Real w, Real h, Real* error = nullptr)
+		{
+			return NDer2_uw<N>(f, u, w, h, h, error);
 		}
 		template <int N>
 		static VectorN<Real, N> NDer2_uw(const IParametricSurfaceRect<N>& f, Real u, Real w, Real* error = nullptr)
